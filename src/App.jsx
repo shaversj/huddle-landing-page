b56ef01ac49e105fbdfa8b37efce1dc3e428649d
Josh Shavers <shaversj@gmail.com>
2024-10-24 08:48:14 -0500

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-function App() {
-  const descriptions = [
-    {
-      title: "Grow Together",
-      description: "Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.",
-      imagePath: "/illustration-grow-together.svg",
-      alt: "Grow Together",
-    },
-    {
-      title: "Flowing Conversation",
-      description: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-      imagePath: "/illustration-flowing-conversation.svg",
-      alt: "Flowing Conversation",
-    },
-    {
-      title: "Your Users",
-      description: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
-      imagePath: "/illustration-your-users.svg",
-      alt: "Your Users",
-    },
-  ];
+import { companyInformation, features } from "./constants/constants.ts";
 
+function App() {
   return (
     <>
       <div>
@@ -39,9 +20,8 @@ function App() {
             <img className={"h-[30.125rem] w-[42.5rem]"} src="/illustration-mockups.svg" alt="Screen Mockups" />
           </section>
         </div>
-
         <section className={"space-y-10"}>
-          {descriptions.map((description, index) => (
+          {features.map((description, index) => (
             <article key={index} className={"mx-[104px] mt-[160px] flex h-[440px] items-center gap-x-[168px] border pl-[109px] pr-4 even:flex-row-reverse"}>
               <div>
                 <h2 className={"font-poppins text-[28px] font-semibold leading-[42px] text-very-dark-cyan"}>{description.title}</h2>
@@ -53,8 +33,40 @@ function App() {
             </article>
           ))}
         </section>
+        <footer className={"bg-very-dark-cyan px-[120px] pb-16 pt-[144px] font-openSans text-[14px] leading-6 text-[#DFE4F0]"}>
+          <img className={"h-6 w-[152px]"} src="/logo-footer.svg" alt="Huddle Logo" />
+          <div className={"flex gap-x-[130px] pt-8"}>
+            <div className={"space-y-4"}>
+              {companyInformation.contactInformation.map((contactInformation, index) => (
+                <div key={index} className={"flex gap-x-4"}>
+                  <div className={`flex size-6 items-center ${contactInformation.icon.includes("location") && "pt-1"}`}>
+                    <img src={contactInformation.icon} alt={contactInformation.iconAlt} />
+                  </div>
+                  <p className={"max-w-[339px]"}>
+                    <a>{contactInformation.text}</a>
+                  </p>
+                </div>
+              ))}
+            </div>
 
-        <footer></footer>
+            <nav className={"grid grid-cols-2 grid-rows-[auto,auto,1fr] gap-x-[130px] gap-y-3 text-nowrap"}>
+              {companyInformation.navigation.map((navigation, index) => (
+                <a className={"h-[27px]"} key={index}>
+                  {navigation.title}
+                </a>
+              ))}
+            </nav>
+
+            <div className={"flex flex-col justify-between"}>
+              <div className={"flex gap-x-[11.35px]"}>
+                {companyInformation.socialMedia.map((socialMedia, index) => (
+                  <img key={index} src={socialMedia.icon} alt="Social Media Icon" />
+                ))}
+              </div>
+              <p className={"text-[12px] leading-[18px]"}>© Copyright 2018 Huddle. All rights reserved.</p>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
---
+++ src/constants/constants.ts
@@ -0,0 +1,83 @@
+export const features = [
+  {
+    title: "Grow Together",
+    description: "Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.",
+    imagePath: "/illustration-grow-together.svg",
+    alt: "Grow Together",
+  },
+  {
+    title: "Flowing Conversation",
+    description: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
+    imagePath: "/illustration-flowing-conversation.svg",
+    alt: "Flowing Conversation",
+  },
+  {
+    title: "Your Users",
+    description: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
+    imagePath: "/illustration-your-users.svg",
+    alt: "Your Users",
+  },
+];
+
+export const companyInformation = {
+  contactInformation: [
+    {
+      icon: "/icon-location.svg",
+      iconAlt: "Location Icon",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      icon: "/icon-phone.svg",
+      iconAlt: "Phone Icon",
+      text: "+1-543-123-4567",
+    },
+    {
+      icon: "/icon-email.svg",
+      iconAlt: "Email Icon",
+      text: "example@huddle.com",
+    },
+  ],
+  navigation: [
+    {
+      title: "About Us",
+      link: "#",
+    },
+    {
+      title: "Career",
+      link: "#",
+    },
+    {
+      title: "What We Do",
+      link: "#",
+    },
+    {
+      title: "Blog",
+      link: "#",
+    },
+    {
+      title: "FAQ",
+      link: "#",
+    },
+    {
+      title: "Contact Us",
+      link: "#",
+    },
+  ],
+  socialMedia: [
+    {
+      name: "Facebook",
+      icon: "/icon-facebook.svg",
+      link: "#",
+    },
+    {
+      name: "Twitter",
+      icon: "/icon-twitter.svg",
+      link: "#",
+    },
+    {
+      name: "Instagram",
+      icon: "/icon-instagram.svg",
+      link: "#",
+    },
+  ],
+};
