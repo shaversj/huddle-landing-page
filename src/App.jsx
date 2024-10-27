0c1ebd6411624a2848ca517fd5b67ff5dc33b2f3
Josh Shavers <shaversj@gmail.com>
2024-10-27 10:02:57 -0500

    Add mobile layout for header

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,32 @@ import { companyInformation, features } from "./constants/constants.ts";
 function App() {
   return (
     <>
-      <div>
-        <div className={"bg-[#EDFBFF] bg-[url('/bg-hero-desktop.svg')] px-20"}>
+      <div className={"antialiased"}>
+        <div className={"bg-[#EDFBFF] bg-[url('/bg-hero-mobile.svg')] px-4 lg:bg-[url('/bg-hero-desktop.svg')] lg:px-20"}>
           <header className={"flex items-center pt-12"}>
-            <img className={"h-[31px] w-[200px]"} src="/logo.svg" alt="Huddle Logo" />
-            <button className={"shadow-button-shadow ml-auto h-12 w-[12.5rem] rounded-full bg-white font-openSans text-[0.875rem] font-bold leading-[1.188rem] text-very-dark-cyan"}>Try It Free</button>
+            <img className={"h-4 w-[6.5rem] lg:h-[1.938rem] lg:w-[12.5rem]"} src="/logo.svg" alt="Huddle Logo" />
+            <button className={"ml-auto h-6 w-[6rem] rounded-full bg-white font-openSans text-[0.625rem] font-bold leading-[0.875rem] text-very-dark-cyan shadow-button-shadow lg:h-12 lg:w-[12.5rem] lg:text-[0.875rem] lg:leading-[1.188rem]"}>
+              Try It Free
+            </button>
           </header>
-          <section className={"flex items-center gap-x-[3.375rem] pb-[4.5rem] pt-[4.438rem]"}>
-            <div className={"basis-[40%] space-y-8"}>
-              <h1 className={"font-poppins text-[2.5rem] font-semibold leading-[3.75rem] text-very-dark-cyan"}>Build The Community Your Fans Will Love</h1>
-              <p className={"font-openSans text-[1.125rem] leading-[1.688rem] text-very-dark-cyan"}>
+          <section className={"mx-auto flex w-[304px] flex-col items-center justify-center pb-[2.313rem] pt-[4.438rem] lg:w-full lg:flex-row lg:gap-x-[3.375rem] lg:px-0 lg:pb-[4.5rem]"}>
+            <div className={"flex w-[304px] basis-[40%] flex-col items-center lg:w-full lg:items-start lg:gap-y-8"}>
+              <h1 className={"text-center font-poppins text-[1.5rem] font-semibold leading-[2.25rem] text-very-dark-cyan lg:text-left lg:text-[2.5rem] lg:leading-[3.75rem]"}>Build The Community Your Fans Will Love</h1>
+              <p className={"pt-6 text-center font-openSans leading-6 text-very-dark-cyan lg:pt-0 lg:text-left lg:text-[1.125rem] lg:leading-[1.688rem]"}>
                 Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.{" "}
               </p>
-              <button className={"shadow-hero-button-shadow h-[3.5rem] w-[17.5rem] rounded-full bg-pink text-center font-openSans text-[0.875rem] font-bold leading-[1.188rem] text-white"}>Get Started For Free</button>
+              <button
+                className={"mt-8 h-[2.5rem] w-[15rem] rounded-full bg-pink text-center font-openSans text-xs font-bold leading-4 text-white shadow-hero-button-shadow lg:mt-0 lg:h-[3.5rem] lg:w-[17.5rem] lg:text-[0.875rem] lg:leading-[1.188rem]"}
+              >
+                Get Started For Free
+              </button>
             </div>
-            <img className={"h-[30.125rem] w-[42.5rem]"} src="/illustration-mockups.svg" alt="Screen Mockups" />
+            <img className={"mt-[3.5rem] lg:mt-0 lg:h-[30.125rem] lg:w-[42.5rem]"} src="/illustration-mockups.svg" alt="Screen Mockups" />
           </section>
         </div>
         <section className={"space-y-10 pb-[344px]"}>
           {features.map((description, index) => (
-            <article key={index} className={"shadow-feature-shadow mx-[104px] mt-[160px] flex h-[440px] items-center gap-x-[168px] rounded-2xl pl-[109px] pr-4 even:flex-row-reverse"}>
+            <article key={index} className={"mx-[104px] mt-[160px] flex h-[440px] items-center gap-x-[168px] rounded-2xl pl-[109px] pr-4 shadow-feature-shadow even:flex-row-reverse"}>
               <div>
                 <h2 className={"font-poppins text-[28px] font-semibold leading-[42px] text-very-dark-cyan"}>{description.title}</h2>
                 <p className={"pt-4 font-openSans text-[18px] leading-[27px] text-grayish-blue"}>{description.description}</p>
@@ -33,9 +39,9 @@ function App() {
             </article>
           ))}
         </section>
-        <section className={"shadow-cta-shadow absolute left-1/2 z-10 -ml-[400px] flex h-[280px] w-[800px] -translate-y-2/3 flex-col items-center justify-center rounded-[15px] bg-white"}>
+        <section className={"absolute left-1/2 z-10 -ml-[400px] flex h-[280px] w-[800px] -translate-y-2/3 flex-col items-center justify-center rounded-[15px] bg-white shadow-cta-shadow"}>
           <p className={"font-poppins text-[32px] font-semibold leading-[60px] text-very-dark-cyan"}>Ready To Build Your Community?</p>
-          <button className={"shadow-cta-button-shadow mt-8 h-[80px] w-[400px] rounded-full bg-pink text-center font-openSans text-[20px] font-bold leading-[30px] text-white"}>Get Started For Free</button>
+          <button className={"mt-8 h-[80px] w-[400px] rounded-full bg-pink text-center font-openSans text-[20px] font-bold leading-[30px] text-white shadow-cta-button-shadow"}>Get Started For Free</button>
         </section>
         <footer className={"z-20 bg-very-dark-cyan px-[120px] pb-16 pt-[144px] font-openSans text-[14px] leading-6 text-[#DFE4F0]"}>
           <img className={"h-6 w-[152px]"} src="/logo-footer.svg" alt="Huddle Logo" />
