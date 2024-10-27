27d205b491dbaece9ee43b03ab17a297fafe9f29
Josh Shavers <shaversj@gmail.com>
2024-10-27 13:09:04 -0500

    Add layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ function App() {
   return (
     <>
       <div className={"antialiased"}>
-        <div className={"bg-[#EDFBFF] bg-[url('/bg-hero-mobile.svg')] px-4 lg:bg-[url('/bg-hero-desktop.svg')] lg:px-20"}>
+        <main className={"bg-[#EDFBFF] bg-[url('/bg-hero-mobile.svg')] px-4 lg:bg-[url('/bg-hero-desktop.svg')] lg:px-20"}>
           <header className={"flex items-center pt-12"}>
             <img className={"h-4 w-[6.5rem] lg:h-[1.938rem] lg:w-[12.5rem]"} src="/logo.svg" alt="Huddle Logo" />
             <button className={"ml-auto h-6 w-[6rem] rounded-full bg-white font-openSans text-[0.625rem] font-bold leading-[0.875rem] text-very-dark-cyan shadow-button-shadow lg:h-12 lg:w-[12.5rem] lg:text-[0.875rem] lg:leading-[1.188rem]"}>
               Try It Free
             </button>
           </header>
+
           <section className={"mx-auto flex w-[19rem] flex-col items-center justify-center pb-[2.313rem] pt-[4.438rem] lg:w-full lg:flex-row lg:gap-x-[3.375rem] lg:px-0 lg:pb-[4.5rem]"}>
             <div className={"flex w-[19rem] basis-[40%] flex-col items-center lg:w-full lg:items-start lg:gap-y-8"}>
               <h1 className={"text-center font-poppins text-[1.5rem] font-semibold leading-[2.25rem] text-very-dark-cyan lg:text-left lg:text-[2.5rem] lg:leading-[3.75rem]"}>Build The Community Your Fans Will Love</h1>
@@ -25,8 +26,9 @@ function App() {
             </div>
             <img className={"mt-[3.5rem] lg:mt-0 lg:h-[30.125rem] lg:w-[42.5rem]"} src="/illustration-mockups.svg" alt="Screen Mockups" />
           </section>
-        </div>
-        <section className={"mx-5 space-y-10 pb-[21.5rem] lg:mx-0"}>
+        </main>
+
+        <section className={"mx-5 space-y-10 pb-[14.313rem] lg:mx-0 lg:pb-[21.5rem]"}>
           {features.map((description, index) => (
             <article
               key={index}
@@ -44,29 +46,33 @@ function App() {
             </article>
           ))}
         </section>
-        <section className={"absolute left-1/2 z-10 mx-3 -ml-[176px] flex h-[176px] min-w-[352px] -translate-y-2/3 flex-col items-center justify-center rounded-[15px] bg-white px-4 shadow-cta-shadow lg:-ml-[400px] lg:h-[280px] lg:w-[800px] lg:px-0"}>
-          <p className={"font-poppins text-[18px] font-semibold leading-[30px] text-very-dark-cyan lg:text-[32px] lg:leading-[60px]"}>Ready To Build Your Community?</p>
-          <button className={"mt-6 h-[40px] w-[200px] rounded-full bg-pink text-center font-openSans text-[10px] font-bold leading-[15px] text-white shadow-cta-button-shadow lg:mt-8 lg:h-[80px] lg:w-[400px] lg:text-[20px] lg:leading-[30px]"}>
-            Get Started For Free
-          </button>
+
+        <section className={"relative mx-4 lg:mx-0"}>
+          <div className={"absolute left-1/2 z-10 flex h-[176px] w-full -translate-x-1/2 -translate-y-2/3 flex-col items-center justify-center rounded-[15px] bg-white shadow-cta-shadow lg:h-[280px] lg:w-[800px]"}>
+            <p className={"font-poppins text-[18px] font-semibold leading-[30px] text-very-dark-cyan lg:text-[32px] lg:leading-[60px]"}>Ready To Build Your Community?</p>
+            <button className={"mt-6 h-[40px] w-[200px] rounded-full bg-pink text-center font-openSans text-[10px] font-bold leading-[15px] text-white shadow-cta-button-shadow lg:mt-8 lg:h-[80px] lg:w-[400px] lg:text-[20px] lg:leading-[30px]"}>
+              Get Started For Free
+            </button>
+          </div>
         </section>
-        <footer className={"z-20 bg-very-dark-cyan px-4 pb-[42px] pt-[124px] font-openSans text-[14px] leading-6 text-[#DFE4F0] lg:px-[120px] lg:pb-16 lg:pt-[144px]"}>
-          <img className={"h-6 w-[152px]"} src="/logo-footer.svg" alt="Huddle Logo" />
-          <div className={"flex flex-col gap-x-[130px] pt-8 lg:flex-row"}>
+
+        <footer className={"z-20 bg-very-dark-cyan px-4 pb-[2.625rem] pt-[7.75rem] font-openSans text-[0.875rem] leading-6 text-[#DFE4F0] lg:px-[7.5rem] lg:pb-16 lg:pt-[9rem]"}>
+          <img className={"h-6 w-[9.5rem]"} src="/logo-footer.svg" alt="Huddle Logo" />
+          <div className={"flex flex-col gap-x-[8.125rem] pt-8 lg:flex-row"}>
             <div className={"space-y-6 lg:space-y-4"}>
               {companyInformation.contactInformation.map((contactInformation, index) => (
                 <div key={index} className={"flex w-full gap-x-4"}>
                   <div className={`flex size-6 items-center justify-start ${contactInformation.icon.includes("location") && "pt-1"}`}>
                     <img src={contactInformation.icon} alt={contactInformation.iconAlt} />
                   </div>
-                  <p className={"w-[298px] lg:max-w-[339px]"}>
+                  <p className={"w-[18.625rem] lg:max-w-[21.188rem]"}>
                     <a>{contactInformation.text}</a>
                   </p>
                 </div>
               ))}
             </div>
 
-            <nav className={"grid gap-x-[130px] gap-y-3 text-nowrap pt-[41px] lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:pt-0"}>
+            <nav className={"grid gap-x-[8.125rem] gap-y-3 text-nowrap pt-[2.563rem] lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:pt-0"}>
               {companyInformation.navigation.map((navigation, index) => (
                 <a className={"h-[27px]"} key={index}>
                   {navigation.title}
@@ -75,12 +81,12 @@ function App() {
             </nav>
 
             <div className={"flex flex-col items-center gap-y-6 pt-6 lg:items-start lg:justify-between lg:gap-y-0 lg:pb-0 lg:pt-0"}>
-              <div className={"flex gap-x-[11.35px]"}>
+              <div className={"flex gap-x-[0.709rem]"}>
                 {companyInformation.socialMedia.map((socialMedia, index) => (
                   <img key={index} src={socialMedia.icon} alt="Social Media Icon" />
                 ))}
               </div>
-              <p className={"text-[12px] leading-[18px]"}>© Copyright 2018 Huddle. All rights reserved.</p>
+              <p className={"text-[0.75rem] leading-[1.125rem]"}>© Copyright 2018 Huddle. All rights reserved.</p>
             </div>
           </div>
         </footer>
